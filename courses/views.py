5b598b757f2929df78dc478e2a982e4d9c493c4e
Lesley Boyd <lesley.a.boyd1@gmail.com>
2019-02-17 14:01:30 -0600

    user can add update and delete courses

--- courses/views.py
+++ courses/views.py
@@ -1,6 +1,8 @@
 from django.urls import reverse_lazy
 from django.views.generic.list import ListView
-from djnago.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+
 
 from .models import Course
 
@@ -53,11 +55,13 @@ class OwnerEditMixin(object):
         return super(OwnerEditMixin, self).form_valid(form)
 
 
-class OwnerCourseMixin(OwnerMixin):
+class OwnerCourseMixin(OwnerMixin, LoginRequiredMixin):
     '''inherits OwnerMixin and provides the Course model for querysets
     '''
 
     model = Course
+    fields = ['subject', 'title', 'slug', 'overview']
+    success_url = reverse_lazy('manage_course_list')
 
 
 class OwnerCourseEditMixin(OwnerCourseMixin, OwnerEditMixin):
@@ -66,7 +70,7 @@ class OwnerCourseEditMixin(OwnerCourseMixin, OwnerEditMixin):
     '''
 
     fields = ['subject', 'title', 'slug', 'overview']
-    success_url = reverse_lazy('manage_coure_list')
+    success_url = reverse_lazy('manage_course_list')
     template_name = 'courses/manage/course/form.html'
 
 
@@ -77,25 +81,29 @@ class ManageCourseListView(OwnerCourseMixin, ListView):
     template_name = 'courses/manage/course/list.html'
 
 
-class CourseCreateView(OwnerCourseEditMixin, CreateView):
+class CourseCreateView(PermissionRequiredMixin, OwnerCourseEditMixin, CreateView):
     '''uses a modelform to create a new course object, uses the fields from OwnerCourseEditMixin to build a model form and subclasses CreateView
     '''
 
-    pass
+    permission_required = 'courses.add_course'
 
 
-class CoureUpdateView(OwnerCourseEditMixin, UpdateView):
+class CourseUpdateView(PermissionRequiredMixin, OwnerCourseEditMixin, UpdateView):
     '''allows editing an existing course object.  Inherits from OwnerCourseEditMixin and UpdateView
     '''
 
-    pass
+    permission_required = 'courses.change_course'
 
 
-class CourseDeleteView(OwnerCourseMixin, DeleteView):
+class CourseDeleteView(PermissionRequiredMixin, 
+                       OwnerCourseMixin, 
+                       DeleteView):
     '''inherits from OwnerCourseMixin and generic DeleteView.  Defines success url for redirect
     '''
 
-    template_name = 'course/manage/course/delete.html'
+    template_name = 'courses/manage/course/delete.html'
     success_url = reverse_lazy('manage_course_list')
+    permission_required = 'courses.delete_course'
+
 
 
